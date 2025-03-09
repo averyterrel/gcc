c429a1720d13d50babf672cf5feed389a843d653
tayoky <tayoky4848@gmail.com>
2025-03-09 18:58:09 +0100

    add config for stanix

---
+++ gcc/config/stanix.h
@@ -0,0 +1,31 @@
+/* Useful if you wish to make target-specific GCC changes. */
+#undef TARGET_STANIX
+#define TARGET_STANIX 1
+
+/* Default arguments you want when running your
+   i686-stanix-gcc/x86_64-stanix-gcc toolchain */
+#undef LIB_SPEC
+#define LIB_SPEC "-lc" /* link against C standard library */
+
+/* Files that are linked before user code.
+   The %s tells GCC to look for these files in the library directory. */
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+
+/* Files that are linked after user code. */
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+
+#undef PID_TYPE
+#define PID_TYPE long
+
+/* Additional predefined macros. */
+#undef TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS()      \
+  do {                                \
+    builtin_define ("__stanix__");      \
+    builtin_define ("__unix__");      \
+    builtin_assert ("system=stanix");   \
+    builtin_assert ("system=unix");   \
+    builtin_assert ("system=posix");   \
+  } while(0);
